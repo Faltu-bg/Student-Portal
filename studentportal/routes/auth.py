821e6a1a0e204bda504b7713ee73dcd4fbfa54e8
Faltu <yadavharsh9103@gmail.com>
2026-04-26 11:16:52 +0530

    Added import of the Model table

--- studentportal/routes/auth.py
+++ studentportal/routes/auth.py
@@ -1,2 +1,31 @@
-def atime():
-    print("hello")
+from flask import Blueprint,request,flash,redirect,render_template
+from flask_login import login_required,logout_user,login_user
+from studentportal.models import Student,Teacher
+
+auth_bp=Blueprint("auth",__name__)
+
+@auth_bp.route("/login",methods=['GET','POST'])
+def login():
+    if request.method=='POST':
+        username = request.form.get("username")
+        password = request.form.get("password")
+        user=Student.query.filter_by(sno=username).first()
+        if not user:
+            user=Teacher.query.filter_by(name=username).first()
+        if user:
+            if password == user.hashed_password:
+                login_user(user)
+                return redirect("/home")
+            else:
+
+                flash('Wrong Password')
+        else:
+
+            print("User not found")
+    return render_template("login.html")
+
+@auth_bp.route("/logout",methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    print("successfull")
--- studentportal/routes/routes.py
+++
@@ -1,2 +0,0 @@
-def atime():
-    print("hello")
