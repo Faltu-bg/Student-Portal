1393af4b60466dbbecac6e2bef72fae2d1575c8d
Faltu <yadavharsh9103@gmail.com>
2026-04-27 22:19:08 +0530

    Changed the homepage,result and page.py

--- studentportal/routes/page.py
+++ studentportal/routes/page.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,request
 from studentportal.models import *
 from flask_login import login_required,current_user
 from studentportal.db import db
@@ -8,27 +8,61 @@ student_bp=Blueprint("student",__name__)
 @student_bp.route("/")
 @login_required
 def home():
-   
-    return render_template("home.html")
+    student = Student.query.get(current_user.sno)
+    enrollments = Enroll.query.filter_by(student_id=student.sno).all()
+    marks = Marks.query.filter_by(student_id=student.sno).all()
+    payments = Payment.query.filter_by(student_id=student.sno).all()
+    sem_status = {}
+
+    for p in payments:
+        if p.sem not in sem_status:
+            sem_status[p.sem] = "pending"
+
+        if p.status == "success":
+            sem_status[p.sem] = "paid"
+    avg_marks = 0
+    if marks:
+        avg_marks = sum(
+            (m.intmarks or 0) + (m.extmarks or 0) + (m.midmarks or 0)
+            for m in marks
+        ) / len(marks)
+
+    return render_template(
+        "home.html",
+        student=student,
+        enrollments=enrollments,
+        marks=marks,
+        sem_status=sem_status,
+        payments=payments,
+        avg_marks=avg_marks
+    )
+
 
 @student_bp.route("/result")
 @login_required
 def result():
+
     student_id = current_user.sno
+    selected_sem = request.args.get("sem", type=int)
 
     student = Student.query.get(student_id)
-    if not student:
-        return "Student not found", 404
 
-    marks_data = db.session.query(Marks, Course).join(Course).filter(
+    query = db.session.query(Marks, Course).join(Course).filter(
         Marks.student_id == student_id
-    ).all()
+    )
 
-    sems = sorted({course.sem for _, course in marks_data})
+    if selected_sem:
+        query = query.filter(Course.sem == selected_sem)
+
+    marks_data = query.all()
+
+    all_sems = db.session.query(Course.sem).distinct().all()
+    all_sems = sorted([s[0] for s in all_sems])
 
     return render_template(
         "result.html",
         student=student,
         marks_data=marks_data,
-        sems=sems
+        sems=all_sems,
+        selected_sem=selected_sem
     )
